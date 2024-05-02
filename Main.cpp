3b1f730fc004e4fbce474ca7e16f56c2fa38ab30
a <a>
2024-05-02 14:30:49 +0200

    Try implement imgui

--- Main.cpp
+++ Main.cpp
@@ -59,13 +59,31 @@ int main(int, char**)
     {
         processInput(window);
 
+        //-------------------------------------------Imgui
+		// Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow(); // Show demo window! :)
+        //-------------------------------------------
+
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   //Change la couleur de clean de l'écran
         glClear(GL_COLOR_BUFFER_BIT);   //Clean l'écran avec la couleur 
 
+        //-------------------------------------------Imgui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        //-------------------------------------------
+
         glfwSwapBuffers(window);
         glfwPollEvents();
+
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
