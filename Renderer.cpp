c83ff68a5d421ef3815b03c8c9781d0f2802646f
a <a>
2024-05-02 18:13:12 +0200

    WIp rendering

--- Renderer.cpp
+++ Renderer.cpp
@@ -6,9 +6,14 @@ void Renderer::init()
 {
 	//Points
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
 	//-----------------Vertex buffer
@@ -16,13 +21,19 @@ void Renderer::init()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);		//Et on bind ce buffer dans GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Copie les données dans le buffer
 	//--------------------------
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//-----------------Shader---------
+	/*
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"out vec4 vertexColor; \n"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 		"}\0";
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -41,9 +52,10 @@ void Renderer::init()
 
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
+		"in vec4 vertexColor;"
 		"void main()\n"
 		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"   FragColor = vertexColor;\n"
 		"}\n\0";
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -58,19 +70,20 @@ void Renderer::init()
 	}
 
 	//-----------------Shader Program---------
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	shader_program_ = glCreateProgram();
+	glAttachShader(shader_program_, vertexShader);
+	glAttachShader(shader_program_, fragmentShader);
+	glLinkProgram(shader_program_);
+
+	glGetProgramiv(shader_program_, GL_LINK_STATUS, &success);
 	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		glGetProgramInfoLog(shader_program_, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
-	glUseProgram(shaderProgram);
+	glUseProgram(shader_program_);
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader);*/
 
 	//--------------------------------------------------------------------
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -81,10 +94,13 @@ void Renderer::init()
 
 	// 1. bind Vertex Array Object
 	glBindVertexArray(VAO);
-	// 2. copy our vertices array in a buffer for OpenGL to use
+	// 2. copy our vertices array in a vertex buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3. then set our vertex attributes pointers
+	// 3. copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 4. then set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -100,8 +116,11 @@ void Renderer::update(float dt)
 void Renderer::draw()
 {
 	//Draw object
-	glUseProgram(shaderProgram);
+	shader_.use();
+
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glPolygonMode(GL_FRONT_AND_BACK, render_mode_ ? GL_LINE :  GL_FILL );
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 
 }
--- Renderer.h
+++ Renderer.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <memory>
 
+#include "Shader.h"
+
+enum RenderMode
+{
+	Flat,
+	Wireframe,
+};
+
 class Renderer
 {
 public:
@@ -8,10 +16,18 @@ public:
 	void update(float dt);
 	void draw();
 
+	void set_render_mode(RenderMode n_mode) { render_mode_ = n_mode; }
+
 private:
 	unsigned int VBO;	//Créer un ID pour le vertex buffer
 	unsigned int VAO;
-	unsigned int shaderProgram;
+	unsigned int EBO;
+
+	unsigned int shader_program_;
+
+	Shader shader_{"Lit.vert", "Lit.frag"};
+
+	RenderMode render_mode_ = Flat;
 
 
 };
